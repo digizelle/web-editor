7e0695f153b747826cab6b0ee91d31384cc2e2b2
theotime2005 <theotime.berthod@epitech.digital>
2025-04-27 12:31:06 +0200

    feat: create editor page

---
+++ src/views/EditorView.vue
@@ -0,0 +1,50 @@
+<script setup>
+import MonacoEditor from "monaco-editor-vue3";
+import {ref} from "vue";
+
+const javascriptText = ref("");
+const htmlText = ref("");
+const cssText = ref("");
+
+function updateJavascript(value) {
+  javascriptText.value = value;
+}
+
+function updateHtml(value) {
+  htmlText.value = value;
+}
+
+function updateCss(value) {
+  cssText.value = value;
+}
+</script>
+
+<template>
+  <h1>Titre</h1>
+  <h2>Javascript</h2>
+  <MonacoEditor
+    v-model="javascriptText"
+    language="javascript"
+    theme="vs-dark"
+    :options="{ automaticLayout: true, accessibilitySupport: true}"
+    @update:modelValue="updateJavascript"
+  />
+  <h2>HTML</h2>
+  <MonacoEditor
+    v-model="htmlText"
+    language="html"
+    theme="vs-dark"
+    :options="{ automaticLayout: true, accessibilitySupport: true}"
+    @update:modelValue="updateHtml"/>
+  <h2>CSS</h2>
+  <MonacoEditor
+    v-model="cssText"
+    language="css"
+    theme="vs-dark"
+    :options="{ automaticLayout: true, accessibilitySupport: true}"
+    @update:modelValue="updateCss"/>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <p>Si vous êtes sur cette page, c'est que vous n'avez bpas bien cliqué.</p>
-  </main>
-</template>
