3de3fc68243febcb32257f6d33420b45eb47f59e
theotime2005 <theotime.berthod@epitech.digital>
2025-09-04 14:10:37 +0200

    feat: create page for home

---
+++ src/pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Web editor</h1>
+  <p>Ce site est l'éditeur web de l'association Digizelle. Si vous voyez cette page, c'est que vous n'êtes pas sur une page disponible.</p>
+  <p>Vous pouvez retourner à la <router-link to="/">page d'accueil</router-link> ou consulter la <router-link to="/pages">liste des pages disponibles</router-link>.</p>
+</template>
+
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import {createRouter, createWebHashHistory} from "vue-router";
 import CodePage from "./pages/CodePage.vue";
 import CreatePage from "./pages/CreatePage.vue";
+import Home from "./pages/Home.vue";
 
 const routes = [
+    {path: '/', component: Home},
     { path: '/code/:id', component: CodePage, props: true },
     { path: '/create', component: CreatePage }
 ]
